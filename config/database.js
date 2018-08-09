7e176d05d224ddb29c3371b8a15a5e916472f119
Nhan Phan <lenhan1000@gmail.com>
2018-08-09 11:17:06 -0400

    Fix db url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret':'eylmao',
-  'database': 'mongodb://<admin>:<P@ssw0rd>@ds117422.mlab.com:17422/fetch-coins-server'
+  'database': 'mongodb://admin:P%40ssw0rd@ds117422.mlab.com:17422/fetch-coins-server'
 };
